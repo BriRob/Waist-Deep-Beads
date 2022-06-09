c52b44df4c8e582be7368f6fd26094d938d627ba
Briana Robinson <briana.n.robinson@gmail.com>
2022-06-09 13:27:24 -0400

    added demo login functionality, changed nav bar to only see logout button if user is logged in

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,43 +1,68 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 
 const NavBar = () => {
+  const sessionUser = useSelector((state) => state.session?.user);
+
+  let navbar;
+  if (sessionUser) {
+    navbar = (
+      <div>
+        <LogoutButton />
+      </div>
+    );
+  } else {
+    navbar = (
+      <div>
+        <NavLink to="/login" exact={true} activeClassName="active">
+          Login
+        </NavLink>
+
+        <NavLink to="/sign-up" exact={true} activeClassName="active">
+          Sign Up
+        </NavLink>
+      </div>
+    );
+  }
+
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+          <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </li>
+
         <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink to="/users" exact={true} activeClassName="active">
             Users
           </NavLink>
         </li>
         <li>
-          <NavLink to='/waistbeads/new' exact={true} activeClassName='active'>
+          <NavLink to="/waistbeads/new" exact={true} activeClassName="active">
             New Post
+          </NavLink>
+        </li>
+        {navbar}
+        {/* <li>
+          <NavLink to="/login" exact={true} activeClassName="active">
+            Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/sign-up" exact={true} activeClassName="active">
+            Sign Up
           </NavLink>
         </li>
         <li>
           <LogoutButton />
-        </li>
+        </li> */}
       </ul>
     </nav>
   );
-}
+};
 
 export default NavBar;

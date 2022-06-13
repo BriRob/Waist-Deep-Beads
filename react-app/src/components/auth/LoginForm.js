fac7fb994d455e10f73bc9866d3d1630afd62e64
Briana Robinson <briana.n.robinson@gmail.com>
2022-06-13 01:11:26 -0400

    added css for login/signup forms

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-import { login } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { login } from "../../store/session";
+import "./LoginForm.css";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onLogin = async (e) => {
@@ -27,50 +28,56 @@ const LoginForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   const loginDemo = async (e) => {
-    e.preventDefault()
-    const email = "demo@aa.io"
-    const password = "password"
+    e.preventDefault();
+    const email = "demo@aa.io";
+    const password = "password";
     const data = await dispatch(login(email, password));
     if (data) {
       setErrors(data);
     }
-
-  }
+  };
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
+    <div className="loginformdiv">
+      <form onSubmit={onLogin}>
+        <h1 className="loginTitle">Log In</h1>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div className="login-grid">
+          {/* <div> */}
+            <label htmlFor="email">Email*</label>
+            <input
+              name="email"
+              type="text"
+              // placeholder="Email"
+              value={email}
+              onChange={updateEmail}
+            />
+          {/* </div> */}
+          {/* <div> */}
+            <label htmlFor="password">Password*</label>
+            <input
+              name="password"
+              type="password"
+              // placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+            />
+          {/* </div> */}
+        </div>
+        <button type="submit">Login</button>
         <button onClick={loginDemo}>Login as Demo User</button>
-      </div>
-    </form>
+        <Link to="/sign-up">Become a Member</Link>
+        <div>*Required</div>
+      </form>
+    </div>
   );
 };
 
--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { login, signUp } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { login, signUp } from "../../store/session";
+import "./SignUpForm.css";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
@@ -17,21 +18,20 @@ const SignUpForm = () => {
     if (password === repeatPassword) {
       const data = await dispatch(signUp(username, email, password));
       if (data) {
-        setErrors(data)
+        setErrors(data);
       }
     }
   };
 
   const loginDemo = async (e) => {
-    e.preventDefault()
-    const email = "demo@aa.io"
-    const password = "password"
+    e.preventDefault();
+    const email = "demo@aa.io";
+    const password = "password";
     const data = await dispatch(login(email, password));
     if (data) {
       setErrors(data);
     }
-
-  }
+  };
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
@@ -50,56 +50,63 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-      <button onClick={loginDemo}>Login as Demo User</button>
-    </form>
+    <div className="signupformdiv">
+      <form onSubmit={onSignUp}>
+        <h1 className="signUpTitle">Sign Up</h1>
+
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div className="signup-grid">
+          <label>Username*</label>
+          <input
+            type="text"
+            name="username"
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        {/* </div>
+        <div> */}
+          <label>Email*</label>
+          <input
+            type="text"
+            name="email"
+            onChange={updateEmail}
+            value={email}
+          ></input>
+        {/* </div>
+        <div> */}
+          <label>Password*</label>
+          <input
+            type="password"
+            name="password"
+            onChange={updatePassword}
+            value={password}
+          ></input>
+        {/* </div>
+        <div> */}
+          <label>Repeat Password*</label>
+          <input
+            type="password"
+            name="repeat_password"
+            onChange={updateRepeatPassword}
+            value={repeatPassword}
+            required={true}
+          ></input>
+        </div>
+        <button type="submit">Sign Up</button>
+        <button onClick={loginDemo}>Login as Demo User</button>
+        <Link to="/login">Already a Member?</Link>
+        <div>*Required</div>
+
+      </form>
+    </div>
   );
 };
 

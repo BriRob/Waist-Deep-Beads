99cc000e62f710d8877149caa280d3721af32246
Briana Robinson <briana.n.robinson@gmail.com>
2022-07-05 17:16:59 -0500

    added state for results

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function Search() {
-    return (
-        <div>
-            <form>
-                <input type="text" placeholder="search"></input>
-            </form>
-        </div>
-    )
+  const waistbeads = useSelector(
+    (state) => state.waistbeadsReducer?.waistbeads
+  );
+  console.log(waistbeads)
+
+  const [results, setResults] = useState([])
+
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="search"></input>
+      </form>
+    </div>
+  );
 }
 
 export default Search;

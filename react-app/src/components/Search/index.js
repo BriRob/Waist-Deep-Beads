884f3d0902454648a779536eb72d0ebfa8c3e3ca
Briana Robinson <briana.n.robinson@gmail.com>
2022-07-06 10:07:39 -0500

    search functionality working

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
 import { getAllWaistbeadsThunk } from "../../store/waistbeads";
 
 function Search() {
   const dispatch = useDispatch();
+  const history = useHistory();
+
 
   const waistbeads = useSelector(
     (state) => state.waistbeadsReducer?.waistbeads
   );
-  console.log("waistbeads", waistbeads)
+  console.log("waistbeads", waistbeads);
 
-//   const [searching, setSearching] = useState([])
-  const [results, setResults] = useState([])
+    const [searchInput, setSearchInput] = useState('')
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     (async () => {
       await dispatch(getAllWaistbeadsThunk());
-    //   setIsLoading(false);
+      //   setIsLoading(false);
     })();
 
     // return () => dispatch(clearAllWbs());
@@ -24,22 +27,38 @@ function Search() {
 
   const handleChange = (e) => {
     // setSearching(e.target.value)
-    const searching = e.target.value
-    const searchingWbArr = waistbeads.filter(wb => (
+    const searching = e.target.value;
+    // console.log("searching", searching === "");
+    setSearchInput(e.target.value)
+    if (searching === "") {
+      setResults([]);
+    } else {
+      const searchingWbArr = waistbeads.filter((wb) =>
         wb.name.toLowerCase().includes(searching.toLowerCase())
-    ))
-    // console.log("searching waistbeads arr", searchingWbArr)
+      );
+      // console.log("searching waistbeads arr", searchingWbArr)
+      setResults(searchingWbArr);
+    }
+  };
+
+  const toWaistbead = (res) => {
+    history.push(`/waistbeads/${res.id}`)
+    setSearchInput("")
+    setResults([])
   }
+
+  console.log('results', results);
 
   return (
     <div>
       <form>
-        <input type="text" placeholder="search" onChange={handleChange}></input>
-        <div className="dynRes">
-            {results.length != 0 && (
-                <div>Hello</div>
-            )}
-        </div>
+        <input type="text" placeholder="search" onChange={handleChange} value={searchInput}className="searchInput"></input>
+        <div className="dynRes">{results.length != 0 && (
+        // <div>Hello</div>
+        results.map((res,idx) => (
+            <div key={idx} onClick={() => toWaistbead(res)}>{res.name}</div>
+        ))
+        )}</div>
       </form>
     </div>
   );
